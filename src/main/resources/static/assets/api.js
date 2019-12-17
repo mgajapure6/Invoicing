dfb73efafb5b4c7f7d05b56a8174f00d0b44ee75
mgajapure6 <mgajapure6@gmail.com>
2019-12-17 17:32:35 +0530

    new

---
+++ src/main/resources/static/assets/api.js
@@ -0,0 +1,3 @@
+// both api is from timesofindia.indiatimes.com
+var fuel_price_api = "https://toibnews.timesofindia.indiatimes.com/fuelprice/nagpur-fuelprice";
+var market_live_data_api = "https://toibnews.timesofindia.indiatimes.com/marketing/homepagedatanew.json";
